20c10431f80d7347bd252193bb04a56f8158a229
SANrio-777 <sanjhana2006@gmail.com>
2026-03-06 10:12:32 +0530

    P4:Plagiarism Detection System

--- src/Week01_02.java
+++ src/Week01_02.java
@@ -2,19 +2,24 @@ import java.util.HashMap;
 
 public class Week01_02 {
 
-    HashMap<String, Integer> usernames = new HashMap<>();
+    HashMap<String, Integer> attempts = new HashMap<>();
 
-    public void suggestAlternatives(String username) {
-        System.out.println("Username not available. Suggestions:");
-        System.out.println(username + "123");
-        System.out.println(username + "_01");
-        System.out.println(username + "_official");
+    public void trackAttempt(String username) {
+
+        if (attempts.containsKey(username)) {
+            attempts.put(username, attempts.get(username) + 1);
+        } else {
+            attempts.put(username, 1);
+        }
+
+        System.out.println(username + " attempts: " + attempts.get(username));
     }
 
     public static void main(String[] args) {
 
         Week01_02 app = new Week01_02();
 
-        app.suggestAlternatives("john");
+        app.trackAttempt("john");
+        app.trackAttempt("john");
     }
 }
